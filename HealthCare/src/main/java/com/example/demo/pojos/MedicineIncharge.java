1bf1c3e3edb63c366a676fbfac0937259ecefe64
Piyush Joshi <impiyushjoshi@gmail.com>
2022-09-08 18:01:38 +0530

    pojo completed. lab/medicine incharge added.

---
+++ HealthCare/src/main/java/com/example/demo/pojos/MedicineIncharge.java
@@ -0,0 +1,25 @@
+package com.example.demo.pojos;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "medicine_incharge_profile")
+public class MedicineIncharge {
+	
+	@Id
+	private String email ; 
+	
+	private String name ; 
+	private long phone_no ; 
+	private String address ; 
+	private LocalDate dob ; 
+	private Gender gender ; 
+	//photopath 
+	
+	private String password ; 
+
+}
